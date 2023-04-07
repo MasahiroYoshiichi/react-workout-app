f43e0234139c91ac2299f26fe48770a40f79a02e
masahiro yoshiichi <rescue.masahiro.441@gmail.com>
2023-04-07 12:14:30 +0900

    トップレイアウト完成

--- src/components/molecules/Navigation.tsx
+++ src/components/molecules/Navigation.tsx
@@ -1,4 +1,5 @@
 import type {FC} from "react";
+// import {Link} from 'react-router-dom'
 
 const Navigation: FC = () => {
     return (
--- src/components/organisms/Top.tsx
+++ src/components/organisms/Top.tsx
@@ -6,13 +6,13 @@ const Top: FC = () => {
         <>
             <div className="w-screen h-screen">
                 <Navigation></Navigation>
-                <div className="flex h-full w-full items-center ml-10">
-                    {/* // style={{backgroundImage: `url(${Image})`}}> */}
-                    <div className="flex-col font-sans text-5xl">
+                <div className="flex h-full w-full items-center  bg-cover"
+                     style={{backgroundImage: `url(/images/EXTop.png)`}}>
+                    <div className="flex-col ml-10 font-sans text-5xl">
                         最高のワークアウトを全ての人へ
                         <button
                             className="flex mt-5 bg-transparent hover:bg-red-500 text-red-700 font-semibold hover:text-white py-2 px-4 border border-red-500 hover:border-transparent rounded">
-                            Challenge Training
+                            Challenge Workout
                         </button>
                     </div>
                 </div>
